eef25967d892609581e0f1cd91bf2695470e66f1
Ivan <ivan.borisovskiy.95@inbox.ru>
2024-08-18 15:59:33 +0800

    add react testing library, add tests for btn, sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable i18next/no-literal-string */
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+import {
+    renderWithTranslation,
+} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { Sidebar } from './Sidebar';
+
+describe('Sidebar', () => {
+    test('Test render', () => {
+        renderWithTranslation(<Sidebar />);
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+    });
+
+    test('test toggle', () => {
+        renderWithTranslation(<Sidebar />);
+        const toggleBtn = screen.getByTestId('sidebar-toggle');
+        fireEvent.click(toggleBtn);
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
+    });
+});
