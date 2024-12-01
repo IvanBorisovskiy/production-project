766cd371bdf2ad08ef1acaef704f905a873c458f
borisovskiyIvan <ivan.borisovskiy.95@inbox.ru>
2024-12-01 15:05:37 +0800

    add profile rating features

---
+++ src/features/profileRating/ui/ProfileRating/ProfileRating.tsx
@@ -0,0 +1,67 @@
+import { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+import { RatingCard } from '@/entities/Rating';
+import { useGetProfileRating, useRateProfile } from '../../api/profileRatingApi';
+import { getUserAuthData } from '@/entities/User';
+import { Skeleton } from '@/shared/ui/Skeleton/Skeleton';
+
+export interface ProfileRatingProps {
+    className?: string;
+    profileId: string;
+}
+
+const ProfileRating = (props: ProfileRatingProps) => {
+    const {
+        className,
+        profileId,
+    } = props;
+
+    const { t } = useTranslation();
+    const userData = useSelector(getUserAuthData);
+    const {
+        data: profileRating,
+        isLoading,
+    } = useGetProfileRating({
+        profileId,
+        userId: userData?.id ?? '',
+    });
+    const [rateProfileMutation] = useRateProfile();
+
+    const handleRateProfile = useCallback((starsCount: number, feedback?: string) => {
+        rateProfileMutation({
+            profileId,
+            rate: starsCount,
+            userId: userData?.id || '',
+            feedback,
+        });
+    }, [profileId, rateProfileMutation, userData?.id]);
+
+    const onAccept = useCallback((starsCount: number, feedback?: string) => {
+        handleRateProfile(starsCount, feedback);
+    }, [handleRateProfile]);
+
+    const onCancel = useCallback((starsCount: number) => {
+        handleRateProfile(starsCount);
+    }, [handleRateProfile]);
+
+    const rating = profileRating?.[0];
+
+    if (isLoading) {
+        return <Skeleton width="100%" height={120} />;
+    }
+
+    return (
+        <RatingCard
+            onAccept={onAccept}
+            onCancel={onCancel}
+            rate={rating?.rate}
+            className={className}
+            title={t('Оцените профиль')}
+            hasFeedback
+            feedbackTitle={t('Напишите своё мнение по поводу данного профиля')}
+        />
+    );
+};
+
+export default memo(ProfileRating);
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,7 +1,10 @@
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { getUserAuthData } from '@/entities/User';
 import {
     EditableProfileCard,
 } from '@/features/editableProfileCard';
+import { ProfileRating } from '@/features/profileRating';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Page } from '@/widgets/Page';
 
@@ -11,10 +14,18 @@ interface ProfilePageProps {
 
 const ProfilePage = ({ className }: ProfilePageProps) => {
     const { id } = useParams<{id: string}>();
+    const userData = useSelector(getUserAuthData);
+
+    const visibleRating = id !== userData?.id;
+
+    if (!id) {
+        return null;
+    }
 
     return (
         <Page className={classNames('', {}, [className])}>
             <EditableProfileCard id={id} />
+            {visibleRating && <ProfileRating profileId={id} />}
         </Page>
     );
 };
